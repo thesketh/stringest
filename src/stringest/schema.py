c4173096252d6b8e582b1b4a706994a93b5a9aa1
Travis Hesketh <travis@hesketh.scot>
2022-05-02 21:41:16 +0100

    Added constant/special sources for fields, cleared up logic around nullability checks.

--- src/stringest/schema.py
+++ src/stringest/schema.py
@@ -1,5 +1,6 @@
 """Representations of inbound schemas."""
-from typing import List, Optional, Tuple, Union
+from copy import deepcopy
+from typing import Any, List, Optional, Tuple, Union
 
 # import pyarrow as pa  # type: ignore
 
@@ -8,19 +9,69 @@ from stringest.steps.base import AbstractStep
 from stringest.type_aliases import Value, Success
 
 
+class Constant:  # pylint: disable=too-few-public-methods
+    """
+    A fixed input value for a field. This will be passed
+    to the field in place of a value from the source file.
+
+    Arguments:
+     - `value`: the value of the constant.
+
+    """
+
+    def __init__(self, value: Any):
+        self._value = value
+
+    @property
+    def value(self) -> Any:
+        """The value of the constant."""
+        return deepcopy(self._value)
+
+
+class Special:  # pylint: disable=too-few-public-methods
+    """
+    A special value inferred from the input file. This will be passed
+    to the field in place of a value from the source file.
+
+    Arguments:
+     - `value_type`: This should be one of the following:
+       - `file_name`: the name of the inbound file
+       - `record_index`: the index (zero-based) of the record
+       - `record_number`: the index (one-based) of the record
+
+    """
+
+    def __init__(self, value_type: str):
+        value_type = value_type.lower()
+        if value_type not in {
+            "file_name",
+            "record_index",
+            "record_number",
+        }:
+            raise ValueError(
+                f"Incorrect value for `value_type` ({value_type!r}). "
+                + "Should be one of `{'file_name', 'record_index', 'record_number'}`"
+            )
+        self._value_type = value_type
+
+    def value_type(self) -> str:
+        """The type of special input value."""
+        return self._value_type
+
+
 class Field:
     """
     A field within an inbound schema.
 
     Arguments:
-     - `name`: the inbound name of the field
+     - `name`: the inbound name of the field, a `Constant`, or a `Special`.
      - `steps`: an (optional) `AbstractStep` or list of `AbstractStep`s to apply to
        the field, in sequence. For free-text fields, this may be omitted.
        The first step must accept an optional string, each in sequence must accept
        the return value of the previous step.
      - `parquet_type`: a string indicating the type of the field in the Parquet file.
-     - `outbound_name`: the (optional) name of the field at output time. If not
-       provided, `name` will be used.
+     - `outbound_name`: the name of the field at output time. If `name` is not a string,
+        this must be provided. If not provided and `name` is a string, `name` will be used.
      - `mandatory`: A flag indicating whether a value must be provided for the field
         on input (default: `False`)
      - `nullable`: A flag indicating whether the value is allowed to be null on
@@ -33,7 +84,7 @@ class Field:
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
-        name: str,
+        name: Union[str, Constant, Special],
         steps: Union[None, AbstractStep, List[AbstractStep]] = None,
         parquet_type: str = "string",
         outbound_name: Optional[str] = None,
@@ -43,7 +94,19 @@ class Field:
     ):
         self._name = name
         self._parquet_type = parquet_type
-        self._outbound_name = outbound_name or self._name
+
+        if outbound_name:
+            self._outbound_name = outbound_name
+        elif isinstance(self._name, str):
+            self._outbound_name = self._name
+        else:
+            raise ValueError(
+                "If `name` is not a string, `outbound_name` must be provided"
+            )
+
+        if mandatory and nullable:
+            raise ValueError("A field cannot be both mandatory and nullable")
+
         self._mandatory = mandatory
         self._nullable = nullable
         self._fail_on_error = fail_on_error
@@ -64,8 +127,9 @@ class Field:
             raise TypeError("`steps` must be a step, a list of steps, or `None`")
 
     @property
-    def name(self) -> str:
-        """The name of the inbound field."""
+    def name(self) -> Union[str, Constant, Special]:
+        """The name of the inbound field (or a constant or special value)."""
+        return self._name
 
     @property
     def steps(self) -> List[AbstractStep]:
@@ -120,6 +184,7 @@ class Field:
             return None, False, [message]
 
         messages: List[Message] = []
+        field_success = True
 
         for step in self._steps:
             try:
@@ -132,32 +197,38 @@ class Field:
                     content=f"Unexpected error in {step.name}: {err!r}",
                 )
 
-            if message:
-                messages.append(message)
             if not success:
-                if not message:
+                if message is None:
                     message = Message(
                         status="ERROR", content=f"{step.name} reported failure"
                     )
-                    messages.append(message)
-                break
 
-        field_success = True
-        if value is None and not self._nullable:
-            field_success = False
-
-        output_messages = []
-        for message in messages:
-            if message.status == "INTERNAL_ERROR":
-                field_success = False
-            if message.status == "ERROR":
-                if self._fail_on_error:
+                # Stop processing additional steps if failing on error
+                # or an internal error is encountered.
+                if self._fail_on_error or message.status == "INTERNAL_ERROR":
                     field_success = False
-                else:
-                    message = message.downgrade()
-            output_messages.append(message)
+                    messages.append(message)
+                    break
 
-        return value, field_success, output_messages
+                # If continuing with process, downgrade errors to warnings.
+                if message.status == "ERROR":
+                    message = message.downgrade()
+
+            if message is not None:
+                messages.append(message)
+
+        if value is None and not self._nullable:
+            # This can happen if we don't raise on error and end up with a
+            # null value at the end of the steps.
+            if field_success is True:
+                message = Message(
+                    status="ERROR",
+                    content="Null value in non-nullable field after ingestion",
+                )
+                messages.append(message)
+                field_success = False
+
+        return value, field_success, messages
 
 
 class Schema:
